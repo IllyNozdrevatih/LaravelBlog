f82cd6bb0f29c1768c6f02c4ea262b6ec596e983
Illy <noz2203@mail.ru>
2019-01-20 18:40:33 +0200

    first commit

---
+++ resources/views/blogger/articles/create.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+        {{ Form::open(['route' => ['articles.store'] , 'method' => 'post','files' => true  ]) }}
+            <div class="form-group row">
+                <label for="example-text-input" class="col-2 col-form-label">Title</label>
+                <div class="col-10">
+                    <input class="form-control" name="title" type="text" id="example-text-input">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="exampleTextarea">Description</label>
+                <textarea class="form-control" name="description" id="exampleTextarea" rows="3"></textarea>
+            </div>
+            <div class="form-group">
+                <div><label for="exampleTextarea">Image</label></div>
+                <input name="image" type="file" id="example-text-input">
+            </div>
+            @foreach( $categories as $category)
+                <input name=categories[] type="checkbox" value={{ $category->id }}>{{ $category->name }}
+            @endforeach
+            <div class="mt-2 mb-2">
+                <input type="submit" value="Создать" class="btn btn-primary">
+            </div>
+            {{ Form::close() }}
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        <a href="{{ url()->previous() }}" class="h5 text-primary">Назад</a>
+@endsection
---
+++ resources/views/blogger/articles/edit.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+         {{ Form::open(['route' => ['articles.update',$article] , 'method' => 'put', 'files' => true]) }}
+            <div class="form-group row">
+                <label for="example-text-input" class="col-2 col-form-label">Title</label>
+                <div class="col-10">
+                    <input class="form-control" name="title" type="text" id="example-text-input" value="{{ $article->title }}">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="exampleTextarea">Description</label>
+                <textarea class="form-control" name="description" id="exampleTextarea" rows="3">{{ $article->description }}</textarea>
+            </div>
+            <div class="form-group">
+                <div><label for="exampleTextarea">Image</label></div>
+                <input name="image" type="file" id="example-text-input" value="{{ $article->image }}">
+            </div>
+            @foreach( $categories as $category)
+                <input type="checkbox" name="categories[]" value={{$category->id}} @if(count($article->categories->whereIn('id',$category->id))) checked @endif>{{ $category->name }}
+            @endforeach
+             <div class="mt-2 mb-2">
+                 <input type="submit" value="Создать" class="btn btn-primary">
+             </div>
+            <input type="hidden" name="_method" value="put">
+            @if (count($errors) > 0)
+                 <div class="alert alert-danger">
+                     <ul>
+                         @foreach ($errors->all() as $error)
+                             <li>{{ $error }}</li>
+                         @endforeach
+                     </ul>
+                 </div>
+             @endif
+        {{  Form::close() }}
+        <a href="{{ url()->previous() }}" class="h5 text-primary">Назад</a>
+@endsection
